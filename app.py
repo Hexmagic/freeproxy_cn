41a843f9fd73e7f3db9375144f0a48300ef01174
Hexmagic <191440042@qq.com>
2019-12-30 11:09:33 +0800

    update

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ loop = asyncio.get_event_loop()
 eng = Engin(redis_host="127.0.0.1", redis_port=6379,
             redis_db=0, redis_password="")
 eng.site_list()
+loop.run_until_complete(asyncio.gather(eng.run()))
